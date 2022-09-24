0e46252f97fbcea8c0ff1cce89df0ac1c71ff6a5
duyhuynhdn <duyhuynh.dn@gmail.com>
2022-09-24 15:15:13 +0700

    Save

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
+
 from sklearn.externals import joblib
+#import joblib
 from sklearn.preprocessing import StandardScaler
+#import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -20,7 +23,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -63,6 +66,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+	LOG.info(f"LOG prediction: \n{prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
