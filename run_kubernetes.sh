331741f43d7f52d3a4533355a897a4a7c67322a6
duyhuynhdn <duyhuynh.dn@gmail.com>
2022-09-25 10:36:37 +0700

    Add

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=duyhuynhdn/devops-project4-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy predict-kubernate --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/predict-kubernate 80:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=duyhuynhdn/devops-project4-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+#docker login -u YOUR_USER_NAME --p YOUR_PASSWORD
+docker tag api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
